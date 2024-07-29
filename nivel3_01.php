d09675f6461390a34e6eea7b4eb6f6735892883b
frankpulido <frankpulido@me.com>
2024-07-29 19:10:50 +0200

    tema2 fully accomplished

--- nivel3_01.php
+++ nivel3_01.php
@@ -13,44 +13,57 @@ ESTUDIAR :
 // repasar scope de las variables : globales, etc
 // ojo "passing arguments by reference" : https://www.w3schools.com/php/php_functions.asp
 // ojo definición de valor por defecto en las funciones : https://www.w3schools.com/php/php_functions.asp
-// ojo uso del for each
-// Planteamiento de nivel2_03 con arrays :
+// ojo uso del foreach
 */
 
-echo "Bienvenidos a IT Álgebra" . "\n";
-echo "Hoy usaremos la \"criba de Eratóstenes\" para encontrar los números primos en el intervalo desde el 1 hasta el número que nos indique" . "\n";;
-echo "Indíquemos un número entero no menor de 3 (ya conocemos que 1, 2 y 3 son primos) : " . "\n";;
-
-do {
-    $rango = (int) readline ("Indíquenos hasta que número desea que hagamos la búsqueda : ");
-    if($rango < 3) {echo "El número debe ser mayor de 3. Inténtelo de nuevo.". "\n";}
-} while ($rango < 3);
-
-$primos = [1,2,3];
-
-while ($i < $rango + 1) {
-    $primos = [i];
-    $i++;
-}
-
-
+$rango;
 $numeros = [];
 $purga1 = [];
 $purga2 = [];
+$primos = [];
 
-function purga1($par) {
-    array_push($purga1, $par);
+echo "\n";
+echo "Bienvenidos a IT Álgebra.\n";
+echo "Hoy usaremos la \"criba de Eratóstenes\" para encontrar los números primos en el intervalo desde el 2 hasta el número que nos indique.\n";;
+echo "Debe indicarnos un número entero no menor de 3 (ya conocemos que 2 y 3 son números primos).\n";;
+echo "\n";
+
+do {
+    $rango = (int) readline ("Indíquenos hasta que número desea que hagamos la búsqueda : ");
+    if($rango <= 3) {echo "El número debe ser mayor de 3. Inténtelo de nuevo.\n";}
+} while ($rango <= 3);
+
+
+for ($i = 2; $i <= $rango; $i++) {
+    $numeros[] = $i;
 }
 
-function purga2($imparNoPrimo) {
-    echo "";
+echo "El intervalo de números naturales del que debemos extraer los primos :\n\n";
+print_r($numeros);
+
+echo "\n";
+echo "Resultado de la \"Criba de Eratóstenes\" :\n\n";
+$primos = (eratostenes($numeros)); // Los paréntesis alrededor de la función ya definen que se recibe un array
+print_r($primos);
+
+function eratostenes ($array) {
+    do {
+        sort($array);
+        $primos_purga[] = $array[0];
+        $array = (purga($array));
+        unset($array[0]);
+        sort($array); // unset() elimina el índice 0, sort reindexa todo el array()
+    } while (pow($array[0],2) < $array[count($array)-1]); // Study array internal pointer : https://www.w3schools.com/php/func_array_current.asp
+    return array_merge($primos_purga, $array);
 }
 
-foreach ($numeros as $par) {
-    if ($par % 2 == 0) {
-        purga1($par);
-        unset($numeros[$par]);
+function purga($array) {
+    for ($i = 1; $i < count($array); $i++) {
+        sort($array);
+        if(fmod($array[$i], $array[0]) == 0){unset($array[$i]);} // Mejor trabajar residuo con fmod() que con %, el segundo me da errores
     }
+    sort($array);
+    return($array);
 }
 
 ?>
