dca4be456091f594a9285bbe0897df4faf90ba5d
frankpulido <frankpulido@me.com>
2024-07-31 18:33:16 +0200

    [EXTRA MILE] : prime factorization

--- nivel3_01.php
+++ nivel3_01.php
@@ -19,6 +19,7 @@ ESTUDIAR :
 $rango;
 $numeros = [];
 $primos = [];
+$opcion = -1;
 
 echo "\n";
 echo "Bienvenidos a IT Álgebra.\n";
@@ -44,6 +45,22 @@ echo "Resultado de la \"Criba de Eratóstenes\" :\n\n";
 $primos = (eratostenes($numeros)); // Los paréntesis alrededor de la función ya definen que se recibe un array
 print_r($primos);
 
+echo "\n";
+echo "Ahora que conoce los números primos del rango indicado, le gustaría también conocer\nla descomposición factorial de los números NO primos del conjunto?...\n";
+do {
+    $opcion = (int) readline("[1] : SI , [0] : NO ? : ");
+    if ($opcion != 1 && $opcion != 0) {echo "Opción no válida\n";}
+} while ($opcion != 1 && $opcion != 0);
+
+if ($opcion == 1) {
+    $descomposicion_no_primos_asociativo = descomposicion_factorial ($numeros, $primos);
+    print_r($descomposicion_no_primos_asociativo);
+}
+echo "\nHasta pronto, esperamos que le haya gustado este programa... Volveremos con más y mejor.";
+
+
+// A continuación las funciones usadas
+
 function eratostenes ($array) {
     do {
         sort($array);
@@ -62,6 +79,44 @@ function purga($array) {
     }
     sort($array);
     return($array);
+}
+
+// [EXTRA MILE] : DESCOMPOSICIÓN FACTORIAL DE LOS NO PRIMOS - Aprovecha la ejecución de las funciones anteriores y usa $primos
+function descomposicion_factorial ($numeros, $primos) {
+    $no_primos = []; // contendrá las $keys del resultado
+    $factores = []; // contendrá los factores de cada elemento de $no_primos
+    $factores_asociativo = []; // combina $primos como claves de valores de $factores
+    $descomposicion_no_primos_asociativo = []; // resultado final, combinacion de keys $no_primos con $factores_asociativo
+
+
+    $no_primos = array_diff($numeros, $primos); // sustraemos los primos del conjunto de números escogidos por el usuario
+    sort($no_primos); // reindexamos, aunque no es realmente necesario para hacer el array_combine de keys($no_primos) y sus respectivos $factores
+
+    for ($i = 0; $i < count($no_primos); $i++) {
+        $numero = 0;
+        $numero = $no_primos[$i];
+        unset($factores);
+        for ($j = 0; $j < count($primos); $j++) {
+            $primo = 0;
+            $primo = $primos[$j];
+            $pow_primo = $primo; // ahora está elevado a "1"
+            $factor = 0;
+            $testigo = 0;
+            do {
+                if (fmod($numero, $pow_primo) == 0) {
+                    $factor++; // Si se divide por el primo tendremos un factor de $primo^1 ($factor++ = 1) en el primer ciclo, $primo^2 en el segundo, etc
+                    $testigo = 1;
+                    $pow_primo = pow($primo, ($factor + 1)); // 
+                } else {
+                    $testigo = 0;
+                }
+            } while ($testigo == 1 && $pow_primo <= $numero); // Sin la segunda condición el bucle era infinito !!!
+            $factores[] = $factor;
+        }
+        $factores_asociativo[] = array_combine($primos, $factores);
+    }
+    $descomposicion_no_primos_asociativo = array_combine($no_primos, $factores_asociativo);
+    return $descomposicion_no_primos_asociativo;
 }
 
 ?>
